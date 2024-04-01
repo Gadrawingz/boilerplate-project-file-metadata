e16befd86fc7ede406914d1113331ed072a1abfa
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-04-01 19:56:08 +0200

    Third commit to make api/fileanalyse

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This is the boilerplate for the File Metadata Microservice project.
 
+This is the last challenge to make before earning certification.
+
 >Instructions for building your project can be found at: 
 >https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
--- index.js
+++ index.js
@@ -17,8 +17,25 @@ app.get('/', function (req, res) {
 });
 
 
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
 
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
+// MY STUFFS GOES HERE
+app.post("/api/fileanalyse", 
+upload.single("upfile"), 
+function (request, response) {
+  response.json({
+    name: request.file.originalname,
+    type: request.file.mimetype,
+    size: request.file.size,
+  });
+});
+
+// UNTOUCHABLE CODE
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
